9895b2592e845047ce06ea183e4622e3e0fa749c
nathan <nathan@goodfolk.co.nz>
2017-07-20 14:05:43 +1200

    carousel improvements

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,119 +1,112 @@
 export default () => {
-    console.log('carousel')
-    // carousel elements
-    const carousel = document.querySelector('[data-carousel]')
+  const carouselNew = document.querySelectorAll('[data-carousel-new]')
 
-    if(carousel !== null){ // if the carousel exists
-      var slides = carousel.querySelectorAll('.slide')
-      var slidesTotal = slides.length
-      var currentSlide = 0
+  ;[].forEach.call(carouselNew, function (carousel) {
 
-      // set up dots
-      var dots = document.getElementById('dots')
-      var dhtml = ''
+    // slides
+    let slides = carousel.querySelectorAll('.slide')
+    let totalSlides = slides.length
+    let currentSlide = 0
 
-      for (var i = 0; i < slidesTotal; i++) {
-        dhtml += '<span>' + (i + 1) + '</span>'
-      }
+    // dots
+    let dots = carousel.querySelector('[data-dots]')
+    let dhtml = ''
 
-      dots.innerHTML = dhtml
-
-      dots.firstChild.className = 'active'
-
-      var dbuttons = dots.querySelectorAll('span')
-
-      // navigate to carousel image
-      var goToSlide = function(n){
-        slides[currentSlide].className = 'slide'
-        dbuttons[currentSlide].className = ''
-
-        currentSlide = (n + slidesTotal) % slidesTotal
-        slides[currentSlide].className = 'slide showing'
-        dbuttons[currentSlide].className = 'active'
-      }
-
-      const prevSlide = () => {
-        goToSlide(currentSlide - 1)
-      }
-
-      const nextSlide = () => {
-        goToSlide(currentSlide + 1)
-      }
-
-      var prev = document.getElementById('prev')
-      var next = document.getElementById('next')
-
-      prev.addEventListener('click', function(e){
-        e.preventDefault()
-        prevSlide()
-      })
-
-      next.addEventListener('click', function(e){
-        e.preventDefault()
-        nextSlide()
-      })
-
-      // keyboard event
-      document.onkeydown = function(e){
-        e = e || window.event            
-
-        if (e.keyCode === 37) {
-           // left arrow
-           prevSlide()
-        }
-        else if (e.keyCode === 39) {
-           // right arrow               
-           nextSlide()
-        }
-      }
-
-      // dots
-      /*jshint loopfunc: true */ // needed for jshint to allow function within loop
-      for(var k = 0; k < dbuttons.length; k++){
-        dbuttons[k].onclick = (function(n){
-          return function(){
-            goToSlide(n)
-          }
-        })(k)
-      }
-
-      // touch events
-      var xDown = null
-      var yDown = null
-
-      const handleTouchStart = (e) => {
-        xDown = e.touches[0].clientX
-        yDown = e.touches[0].clientY
-      }
-
-      const handleTouchMove = (e) => {
-        if(!xDown || !yDown){
-          return
-        }
-
-        var xUp = e.touches[0].clientX
-        var yUp = e.touches[0].clientY
-
-        var xDiff = xDown - xUp
-        var yDiff = yDown - yUp
-
-        // detect left and right swipes on the carousel
-        if(Math.abs(xDiff) > Math.abs(yDiff)){
-          if(xDiff > 0){
-            // left swipe
-            nextSlide()  
-          } else {
-            // right swipe
-            prevSlide()
-          }
-        }
-
-        // reset values
-        xDown = null
-        yDown = null
-      }
-
-      carousel.addEventListener('touchstart', handleTouchStart, false)
-      carousel.addEventListener('touchmove', handleTouchMove, false)
+    for (let i = 0; i < totalSlides; i++) {
+      dhtml += `<span>${i}</span>`
     }
+
+    dots.innerHTML = dhtml
+    dots.firstChild.classList.add('active')
+    let dotItem = dots.querySelectorAll('span')
+
+    dots.addEventListener('click', function (e) {
+      var index = Array.prototype.indexOf.call(this.children, e.target)
+      if (index !== -1) goToSlide(index)
+    })
+
+    // carousel navigation
+    const goToSlide = (n) => {
+      slides[currentSlide].classList.remove('showing')
+      dotItem[currentSlide].classList.remove('active')
+
+      currentSlide = (n + totalSlides) % totalSlides
+
+      slides[currentSlide].classList.add('showing')
+      dotItem[currentSlide].classList.add('active')
+    }
+
+    const prevSlide = () => {
+      goToSlide(currentSlide - 1)
+    }
+
+    const nextSlide = () => {
+      goToSlide(currentSlide + 1)
+    }
+
+    let prev = carousel.querySelector('.prev')
+    let next = carousel.querySelector('.next')
+
+    prev.addEventListener('click', (e) => {
+      e.preventDefault()
+      prevSlide()
+    })
+
+    next.addEventListener('click', (e) => {
+      e.preventDefault()
+      nextSlide()
+    })
+
+    // keyboard event
+    carousel.onkeydown = function (e) {
+      if (e.keyCode === 37) {
+        // left arrow
+        prevSlide()
+      }
+      else if (e.keyCode === 39) {
+        // right arrow               
+        nextSlide()
+      }
+    }
+
+    // touch events
+    let xDown = null
+    let yDown = null
+
+    const handleTouchStart = (e) => {
+      xDown = e.touches[0].clientX
+      yDown = e.touches[0].clientY
+    }
+
+    const handleTouchMove = (e) => {
+      if (!xDown || !yDown) {
+        return
+      }
+
+      let xUp = e.touches[0].clientX
+      let yUp = e.touches[0].clientY
+
+      let xDiff = xDown - xUp
+      let yDiff = yDown - yUp
+
+      // detect left and right swipes on the carousel
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+          // left swipe
+          nextSlide()
+        } else {
+          // right swipe
+          prevSlide()
+        }
+      }
+
+      // reset values
+      xDown = null
+      yDown = null
+    }
+
+    carousel.addEventListener('touchstart', handleTouchStart, false)
+    carousel.addEventListener('touchmove', handleTouchMove, false)
+
+  })
 }
